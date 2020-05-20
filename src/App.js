62dae3d02c50b27a76b46ef764cc1308d48767cb
Khoa Nguyen <kh.nguyen0101@gmail.com>
2020-05-19 22:48:10 -0700

    test

--- src/App.js
+++ src/App.js
@@ -30,33 +30,3 @@ function App() {
 }
 
 export default App;
-
-
-import React from "react";
-import "./App.css";
-import Nav from "./Components/Nav/Nav.js";
-import Landing from "./Components/Landing/Landing";
-import Home from "./Pages/Home/Home";
-import Portfolio from "./Pages/Portfolio/Portfolio.js";
-import Gallery from "./Pages/Gallery/Gallery.js";
-import Footer from "./Components/Footer/Footer";
-
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-
-      <Landing />
-
-      <Home />
-
-      <Portfolio />
-
-      <Gallery />
-
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
