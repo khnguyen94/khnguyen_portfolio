8c84d1394c9d1aebd13ac762e3cd4c4cf74212a1
Khoa Nguyen <kh.nguyen0101@gmail.com>
2020-05-19 22:38:02 -0700

    updated app.js with by using and rendering components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./Components/Nav/Nav.js";
+import Landing from "./Components/Landing/Landing";
+import Home from "./Pages/Home/Home";
+import Portfolio from "./Pages/Portfolio/Portfolio.js";
+import Gallery from "./Pages/Gallery/Gallery.js";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+
+      <Landing />
+
+      <Home />
+
+      <Portfolio />
+
+      <Gallery />
+
+      <Footer />
     </div>
   );
 }
