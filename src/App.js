220dad3634ad4ffe3cd687aad6b2ee3ca8103b00
Khoa Nguyen <kh.nguyen0101@gmail.com>
2020-05-19 22:51:19 -0700

    reverted to stock app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React from "react";
+import React from 'react';
 import logo from './logo.svg';
-import "./App.css";
-import Nav from "./Components/Nav/Nav.js";
-import Landing from "./Components/Landing/Landing";
-import Home from "./Pages/Home/Home";
-import Portfolio from "./Pages/Portfolio/Portfolio.js";
-import Gallery from "./Pages/Gallery/Gallery.js";
-import Footer from "./Components/Footer/Footer";
+import './App.css';
 
 function App() {
   return (
@@ -14,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Khoa Nguyen React Portfolio
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
